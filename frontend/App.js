dd76211fe0ae8c76146491f44e662e3eeacc500b
Veysi Yiğit <veysiygt21@gmail.com>
2024-04-15 23:20:56 +0300

    created react native project

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+});
